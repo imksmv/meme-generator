aaf356f3c312a56c3899509194d3a62a52ef7642
imksmv <98290303+imksmv@users.noreply.github.com>
2022-06-14 23:33:11 +0100

    Added ability to add top and bottom texts to the meme image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -9,14 +9,22 @@ const Meme = () => {
         randomImage: "",
     });
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setMeme((prevMeme) => ({
+            ...prevMeme,
+            [name]: value,
+        }));
+    };
+
     const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
     const getMemeImage = () => {
         const memesArray = allMemeImages.data.memes;
         const rng = Math.floor(Math.random() * memesArray.length);
         const url = memesArray[rng].url;
-        setMeme((prevImage) => ({
-            ...prevImage,
+        setMeme((prevData) => ({
+            ...prevData,
             randomImage: url,
         }));
     };
@@ -29,22 +37,32 @@ const Meme = () => {
                         className="form__input"
                         placeholder="Top text"
                         type="input"
+                        value={meme.topText}
+                        onChange={handleChange}
+                        name="topText"
                     />
                     <input
                         className="form__input"
                         placeholder="Bottom text"
                         type="input"
+                        name="bottomText"
+                        value={meme.bottomText}
+                        onChange={handleChange}
                     />
                     <button className="form__button" onClick={getMemeImage}>
                         Get a new meme image 🖼
                     </button>
                 </div>
                 {meme.randomImage && (
-                    <img
-                        className="main__image"
-                        src={meme.randomImage}
-                        alt="Meme"
-                    />
+                    <div className="meme">
+                        <img
+                            src={meme.randomImage}
+                            className="main__image"
+                            alt="Meme"
+                        />
+                        <h2 className="meme__text top">{meme.topText}</h2>
+                        <h2 className="meme__text bottom">{meme.bottomText}</h2>
+                    </div>
                 )}
             </div>
         </main>
